d101806cc0ba4a6ceec49d732ffb64038afc7b4f
Kirill Kaimusov <kaimusovks@yandex.ru>
2025-01-19 13:36:54 +0500

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+class Team {
+    constructor() {
+        this.members = new Set();
+    }
+
+    add (character) {
+        if (this.members.has(character)) {
+            throw new Error("Персонаж уже добавлен в команду");
+        }
+        this.members.add(character);
+    }
+
+    addAll (...characters) {
+        characters.forEach(character => this.add(character));
+    }
+
+    toArray() {
+        return Array.from(this.members);
+    }
+}
